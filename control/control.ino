c1d50e8a9ec666e0e219f535d9cba32153925bdc
56KbModem <nick-snel@hotmail.com>
2018-01-15 17:09:35 +0100

    added serial loop in control.ino

--- control/control.ino
+++ control/control.ino
@@ -1,23 +1,39 @@
 #include <Stepper.h>
+#include <Serial.h>
 
-#define STEP 200 	// steppermotor step size
-#define HALF_ROT 100	// half rotation
+#define MOTOR_REV 32 	// steppermotor internal step size
+#define FULL_ROT 2048	// stepper makes full rotation in 2048 steps
+
 /* initialize my_stepper */
-Stepper my_stepper1 = Stepper(STEP, 8, 9);
-Stepper my_stepper2 = Stepper(STEP, 10, 11);
+Stepper my_stepper1(MOTOR_REV, 8, 10, 9, 11);
+Stepper my_stepper2(MOTOR_REV, 4, 6, 5, 7);
+
+int serial_data;
 
 void setup()
 {
-	my_stepper1.setSpeed(60);	// set speed of
-	my_stepper2.setSpeed(60);	// steppers in RPM
+	my_stepper1.setSpeed(200);	// set speed of
+	my_stepper2.setSpeed(200);	// steppers in RPM
 
-	int step1_pos = 0;
-	int step2_pos = 0;
+	Serial.begin(9600);		// wait for client serial conn
 }
 
 void loop()
 {
+	if (Serial.available() > 0){
+		serial_data = Serial.read();
 
+		switch(serial_data){
+			case 1:
+				close_routine();
+				break;
+			case 2:
+				open_routine();
+				break;
+			default:
+				continue;
+		}
+	}
 }
 
 
@@ -28,11 +44,12 @@ void close_routine()
 {
 	int s;	// step counter
 
-	for (s=0; s < HALF_ROT; ++s){
+	/* quarter rotation inward */
+	for (s=0; s < FULL_ROT / 4; ++s){
 		my_stepper1.step(1);
-		delay(200);
+		delay(100);
 		my_stepper2.step(1);
-		delay(200);
+		delay(100);
 	}
 }
 
@@ -44,10 +61,11 @@ void open_routine()
 {
 	int s;	// step counter
 
-	for (s=HALF_ROT; s > 0; --s){
+	/* quarter rotation outward */
+	for (s=FULL_ROT / 4; s > 0; --s){
 		my_stepper1.step(1);
-		delay(200);
-		my_stepper2.step(2);
-		delay(200);
+		delay(100);
+		my_stepper2.step(1);
+		delay(100);
 	}
 }
