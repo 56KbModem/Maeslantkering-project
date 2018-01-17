90a062051e4683f32dfc9396c45cc0bd3d734aa0
56KbModem <nick-snel@hotmail.com>
2018-01-17 22:57:39 +0100

    polarity for closing/opening dam, serial messages added

--- control/control.ino
+++ control/control.ino
@@ -11,8 +11,8 @@ int serial_data;
 
 void setup()
 {
-	my_stepper1.setSpeed(200);	// set speed of
-	my_stepper2.setSpeed(200);	// steppers in RPM
+	my_stepper1.setSpeed(400);	// set speed of
+	my_stepper2.setSpeed(400);	// steppers in RPM
 
 	Serial.begin(9600);		// wait for client serial conn
 }
@@ -23,10 +23,12 @@ void loop()
 		serial_data = Serial.read();
 
 		switch(serial_data){
-			case 1:
+			case 'A':
+				Serial.println("CLOSE");
 				close_routine();
 				break;
-			case 2:
+			case 'B':
+				Serial.println("OPEN");
 				open_routine();
 				break;
 		}
@@ -41,12 +43,12 @@ void close_routine()
 {
 	int s;	// step counter
 
-	/* quarter rotation inward */
-	for (s=0; s < FULL_ROT / 4; ++s){
+	/* 1/8 rotation inward */
+	for (s=0; s < FULL_ROT / 8; ++s){
 		my_stepper1.step(1);
-		delay(100);
-		my_stepper2.step(1);
-		delay(100);
+		delay(30);
+		my_stepper2.step(-1);
+		delay(30);
 	}
 }
 
@@ -58,11 +60,11 @@ void open_routine()
 {
 	int s;	// step counter
 
-	/* quarter rotation outward */
-	for (s=FULL_ROT / 4; s > 0; --s){
-		my_stepper1.step(1);
-		delay(100);
+	/* 1/8 rotation outward */
+	for (s=0; s < FULL_ROT / 8; ++s){
+		my_stepper1.step(-1);
+		delay(30);
 		my_stepper2.step(1);
-		delay(100);
+		delay(30);
 	}
 }
