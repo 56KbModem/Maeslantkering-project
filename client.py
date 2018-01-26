49ba137cc685130d107a58a652120ada0e3d5459
56KbModem <nick-snel@hotmail.com>
2018-01-26 12:52:45 +0100

    reformatted client.py

--- client.py
+++ client.py
@@ -3,45 +3,10 @@ import socket
 import os
 from time import strftime, localtime
 
-server1 = ("192.168.42.100", 10000)
+server1 = ("192.168.42.7", 10000)
 server2 = ("192.168.42.200", 10000)
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # tcp socket
 
-# open serial port (9600 baud, 8N1)
-ser_conn = serial.Serial("/dev/ttyUSB0") 
-
-
-while True:
-	ser_in = ser_conn.readline()
-	ser_out = connect_it(ser_in)
-
-	if ser_out == b"A" or ser_out == b"B": # close or open command
-		ser_conn.write(ser_out)
-		connect_it(ser_conn.readline())
-	elif ser_out == b"C": # nothing critical
-		continue
-	else:
-		log_event("Unkown message received: {}".format(ser_out.decode()))
-
-# send message to server
-# if server1 fails, it will
-# try to connect to server2
-def connect_it(serial_message):
-	try:
-		s.connect(server1)
-		log_event("connection established to server 1")
-		s.send(serial_message)
-		ser_out = s.recv(512)
-
-		return ser_out
-
-	except ConnectionRefusedError:
-		log_event("trying failover server2, server 1 doesn't respond")
-		s.connect(server2)
-		s.send(serial_message)
-		ser_out = s.recv(512)
-
-		return ser_out
 
 # check if logfile exists, else create it
 def write_or_append():
@@ -54,9 +19,30 @@ def write_or_append():
 # log server events
 def log_event(message):
 	file_mode = write_or_append()
-	log_file = open('alarm.log', file_mode)
+	log_file = open('client.log', file_mode)
 	time_stamp = strftime("%Y-%m-%d %H:%M:%S", localtime())
 
 	event = time_stamp + ': ' + message + '\n'
 
 	log_file.write(event)
+
+ser_conn = serial.Serial("/dev/ttyUSB0")
+s.connect(server1)
+
+while True:
+	ser_in = ser_conn.readline()
+	print(ser_in)
+
+	s.send(b"hello!\n")
+
+	data = s.recv(512)
+	print(data)
+
+	if data == b"A\n": # close command
+		ser_conn.write(b"A")
+		log_event("kering gaat dicht")
+	elif data == b"B\n": # open command
+		ser_conn.write(b"B")
+		log_event("kering gaat open")
+	else:
+		log_event("Unkown message received: {}".format(data.decode()))
